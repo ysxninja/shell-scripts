f62c975e7a4da2cf610334c05a51d77386031f49
ya-suke <fredluki100@gmail.com>
2021-10-18 22:06:18 +0300

    simplify scripts

--- folder-mount/external-folder-mount.sh
+++ folder-mount/external-folder-mount.sh
@@ -4,10 +4,11 @@
 
 #check if mounted, continue to show directory, 
 #else, || unmount and then remount  and continue to show directory.
-#replace /dev/`sda6` with your disk sda (check with `lsblk`)
-udisksctl mount -b /dev/sda6 /media/yasuke/ || udisksctl unmount /dev/sda6 && udisksctl mount -b /dev/sda6 /media/yasuke/;
+#replace /dev/sda with your disk sda (check with `lsblk`)
+
+udisksctl mount -b /dev/sda /media/$USER/ || udisksctl unmount /dev/sda && udisksctl mount -b /dev/sda /media/$USER/;
 
 #edit with your file launcher ie.nautilus or nemo or pcmanfm or thunar...
-nemo /media/yasuke/Linux/LEARN;
+nemo /path/to/file;
 
 exit;
